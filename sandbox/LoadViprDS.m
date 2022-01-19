f21b20db4ab8c02b35486d25f566d2792d96a7e4
Aaron Ward <award7@wisc.edu>
2022-01-18 21:01:39 -0600

    added separate methods for creating fileDataStores

--- sandbox/LoadViprDS.m
+++ sandbox/LoadViprDS.m
@@ -1,5 +1,105 @@
 classdef LoadViprDS
     
+    % create file datastores
+    methods (Static)
+        
+        function fs = getVelocityMeanFileDataStore(data_directory)
+            arguments
+                data_directory {mustBeFolder};
+            end
+            
+            search_term = 'comp_vd*.dat';
+            num_files = 3;
+            
+            fs = LoadViprDS.getFileDatastore(data_directory, 'SearchTerm', search_term, 'ExpectedNumFiles', num_files);
+        end
+        
+        function fs = getVelocityFileDataStore(data_directory)
+            arguments
+                data_directory {mustBeFolder};
+            end
+            
+            % create velocity file store 
+            search_term = 'ph_*vd*.dat';
+            
+            % 60 total files = 20 frames x 3 directions
+            num_files = 20 * 3;
+            
+            fs = LoadViprDS.getFileDatastore(data_directory, 'SearchTerm', search_term, 'ExpectedNumFiles', num_files);
+        end
+        
+        function fs = getMagDataStore(data_directory)
+            arguments
+                data_directory {mustBeFolder};
+            end
+            
+            search_term = 'MAG.dat';
+            num_files = 1;
+            
+            fs = LoadViprDS.getFileDatastore(data_directory, 'SearchTerm', search_term, 'ExpectedNumFiles', num_files);
+        end
+        
+    end
+    
+    % create datastores by loadin data from files
+    methods (Static)
+        
+        function ds = loadVelocityData(fs, resolution)
+            arguments
+                fs {mustBeUnderlyingType(fs, 'matlab.io.datastore.FileDatastore')};
+                resolution {mustBeInteger, mustBePositive};
+            end
+            
+            % get # of partitions
+            % 60 total files = 20 frames x 3 directions --> do 20 partitions
+            n_partitions = num_files/3;
+           
+            % init data array
+            data = zeros(resolution, resolution, resolution, 3, 20);
+            for i = 1:n_partitions
+               % get a partition
+               sub_ds = fs.partition(n_partitions, i);
+               data(:,:,:,:,i) = LoadViprDS.readDat(sub_ds, resolution);
+            end
+            
+            % place into arrayDatastore
+            % make IterationDimension = to the frame dimension
+            % make ReadSize so it reads all three directions for a given
+            % frame
+            ds = arrayDatastore(data, 'IterationDimension', 5, 'ReadSize', 3);
+        end
+        
+        function ds = loadMagData(fs, resolution)
+            arguments
+                fs {mustBeUnderlyingType(fs, 'matlab.io.datastore.FileDatastore')};
+                resolution {mustBeInteger, mustBePositive};
+            end
+
+            data = LoadViprDS.readDat(fs, resolution);
+            data = single(data);
+            ds = arrayDatastore(data, 'IterationDimension', 3, 'ReadSize', 1);
+        end
+        
+        function ds = loadVelocityMeanData(fs, resolution)
+            arguments
+                fs {mustBeUnderlyingType(fs, 'matlab.io.datastore.FileDatastore')};
+                resolution {mustBeInteger, mustBePositive};
+            end
+            
+            % init data array
+            data = zeros(resolution, resolution, resolution, num_files);
+            
+            data(:,:,:,:) = LoadViprDS.readDat(fs, resolution);
+            
+            % place into arrayDataStore
+            % make IterationDimension = to the direction dimension
+            % make ReadSize so it reads only one direction
+            ds = arrayDatastore(data, 'IterationDimension', 4, 'ReadSize', 1);
+        end
+        
+    end
+    
+    % helper functions
     methods (Static)
         
         function fs = getFileDatastore(data_directory, opts)
@@ -54,76 +154,6 @@ classdef LoadViprDS
             end
         end
         
-        function ds = loadVelocityData(data_directory, resolution)
-            arguments
-                data_directory {mustBeFolder};
-                resolution {mustBeInteger, mustBePositive};
-            end
-            
-            % create file store 
-            search_term = 'ph_*vd*.dat';
-            
-            % 60 total files = 20 frames x 3 directions
-            num_files = 20 * 3;
-            
-            fs = LoadViprDS.getFileDatastore(data_directory, 'SearchTerm', search_term, 'ExpectedNumFiles', num_files);
-            
-            % get # of partitions
-            % 60 total files = 20 frames x 3 directions --> do 20 partitions
-            n_partitions = num_files/3;
-           
-            % init data array
-            data = zeros(resolution, resolution, resolution, 3, 20);
-            for i = 1:n_partitions
-               % get a partition
-               sub_ds = fs.partition(n_partitions, i);
-               data(:,:,:,:,i) = LoadViprDS.readDat(sub_ds, resolution);
-            end
-            
-            % place into arrayDatastore
-            % make IterationDimension = to the frame dimension
-            % make ReadSize so it reads all three directions for a given
-            % frame
-            ds = arrayDatastore(data, 'IterationDimension', 5, 'ReadSize', 3);
-        end
-        
-        function ds = loadMagData(data_directory, resolution)
-            arguments
-                data_directory {mustBeFolder};
-                resolution {mustBeInteger, mustBePositive};
-            end
-            
-            search_term = 'MAG.dat';
-            num_files = 1;
-            fs = LoadViprDS.getFileDatastore(data_directory, 'SearchTerm', search_term, 'ExpectedNumFiles', num_files);
-            
-            data = LoadViprDS.readDat(fs, resolution);
-            data = single(data);
-            ds = arrayDatastore(data, 'IterationDimension', 3, 'ReadSize', resolution);
-        end
-        
-        function ds = loadVelocityMeanData(data_directory, resolution)
-            arguments
-                data_directory {mustBeFolder};
-                resolution {mustBeInteger, mustBePositive};
-            end
-            
-            % create file store
-            search_term = 'comp_vd*.dat';
-            num_files = 3;
-            fs = LoadViprDS.getFileDatastore(data_directory, 'SearchTerm', search_term, 'ExpectedNumFiles', num_files);
-            
-            % init data array
-            data = zeros(resolution, resolution, resolution, num_files);
-            
-            data(:,:,:,:) = LoadViprDS.readDat(fs, resolution);
-            
-            % place into arrayDataStore
-            % make IterationDimension = to the direction dimension
-            % make ReadSize so it reads only one direction
-            ds = arrayDatastore(data, 'IterationDimension', 4, 'ReadSize', 1);
-        end
-        
     end
-    
+
 end
