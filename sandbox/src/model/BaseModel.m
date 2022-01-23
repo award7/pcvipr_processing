7bff2610b7cea1a2e5fb28c3dbe652f1e5638088
Aaron Ward <award7@wisc.edu>
2022-01-23 12:39:15 -0600

    rm BackgroundPhaseCorrection props

--- sandbox/src/model/BaseModel.m
+++ sandbox/src/model/BaseModel.m
@@ -1,130 +1,14 @@
-classdef BaseModel < handle
+ classdef BaseModel < handle
     
     
     % BackgroundPhaseCorrection-specific properties
-    properties (Access = public)
-        Image = 0.5;
-        Vmax = 0.1;
-        CDThreshold = 0.15;
-        NoiseThreshold = 0.15;
-        FitOrder = 2;
-        ApplyCorrection = 1;
-        MagImage;
-        VelocityImage;
-    end
+    
     
     methods (Access = ?BaseController)
         
         function self = BaseModel() 
         end
                
-    end
-    
-    % getters
-    methods
-        
-        function val = get.MagImage(self)
-            val = self.MagImage;
-        end
-        
-        function val = get.VelocityImage(self)
-            val = self.VelocityImage;
-        end
-        
-        function val = get.Velocity(self)
-            val = zeros(320, 320, 320);
-        end
-        
-        
-        
-    end
-    
-    % setters
-    methods
-        
-        function set.MagImage(self, val)
-            % todo: add validation
-            self.MagImage = val;
-        end
-        
-        function set.VelocityImage(self, val)
-            % todo: add validation
-            self.VelocityImage = val;
-        end
-        
-        function set.Image(self, val)
-            try
-                mustBeInRange(val, 0, 1);
-                self.Image = val;
-            catch ME
-                switch ME.identifier
-                    case 'MATLAB:validators:mustBeInRange'
-                        % todo: display error msg
-                end
-            end
-        end
-        
-        function set.Vmax(self, val)
-            try
-                mustBeInRange(val, 0, 1);
-                self.Vmax = val;
-            catch ME
-                switch ME.identifier
-                    case 'MATLAB:validators:mustBeInRange'
-                        % todo: display error msg
-                end
-            end
-        end
-        
-        function set.CDThreshold(self, val)
-            try
-                mustBeInRange(val, 0, 1);
-                self.CDThreshold = val;
-            catch ME
-                switch ME.identifier
-                    case 'MATLAB:validators:mustBeInRange'
-                        % todo: display error msg
-                end
-            end
-        end
-        
-        function set.NoiseThreshold(self, val)
-            try
-                mustBeInRange(val, 0, 1);
-                self.NoiseThreshold = val;
-            catch ME
-                switch ME.identifier
-                    case 'MATLAB:validators:mustBeInRange'
-                        % todo: display error msg
-                end
-            end
-        end
-        
-        function set.FitOrder(self, val)
-            try
-                mustBeInRange(val, 0, 1);
-                self.FitOrder = val;
-            catch ME
-                switch ME.identifier
-                    case 'MATLAB:validators:mustBeInRange'
-                        % todo: display error msg
-                end
-            end
-        end
-        
-        function set.ApplyCorrection(self, val)
-            try
-                mustBeInRange(val, 0, 1);
-                self.ApplyCorrection = val;
-            catch ME
-                switch ME.identifier
-                    case 'MATLAB:validators:mustBeInRange'
-                        % todo: display error msg
-                end
-            end
-        end
-        
-        
     end
     
 end
