b443ccc50584c066853d79d2768e58e69da398b9
Aaron Ward <award7@wisc.edu>
2022-01-22 21:03:53 -0600

    Create OutputParametersModel.m

---
+++ sandbox/src/model/OutputParametersModel.m
@@ -0,0 +1,100 @@
+classdef OutputParametersModel < handle
+    
+    properties (GetAccess = public, SetAccess = private)
+        Study;
+        Subject;
+        ConditionOrVisit;
+        TimePoint;
+        DataSourceName;
+        DatabaseName;
+        DatabaseTables;
+        OutputAsCsv;
+        OutputPath;
+    end
+    
+    % constructor
+    methods (Access = public)
+        
+        function self = OutputParametersModel()
+        end
+        
+    end
+    
+    % setters
+    methods (Access = public)
+        
+        function setStudy(self, val)
+            arguments
+                self;
+                val {mustBeTextScalar};
+            end
+            self.Study = val;
+        end
+        
+        function setSubject(self, val)
+            arguments
+                self;
+                val {mustBeTextScalar};
+            end
+            self.Subject = val;
+        end
+        
+        function setConditionOrVisit(self, val)
+            arguments
+                self;
+                val {mustBeTextScalar};
+            end
+            self.ConditionOrVisit = val;
+        end
+        
+        function setTimePoint(self, val)
+            arguments
+                self;
+                val {mustBeTextScalar};
+            end
+            self.TimePoint = val;
+        end
+        
+        function setDataSourceName(self, val)
+            arguments
+                self;
+                val {mustBeTextScalar};
+            end
+            self.DataSourceName = val;
+        end
+        
+        function setDatabaseName(self, val)
+            arguments
+                self;
+                val {mustBeTextScalar};
+            end
+            self.DatabaseName = val;
+        end
+        
+        function setDatabaseTables(self, val)
+            arguments
+                self;
+                val (1,:) {mustBeText};
+            end
+            self.DatabaseTables = val;
+        end
+
+        function setOutputAsCsv(self, val)
+            arguments
+                self;
+                val (1,1) logical;
+            end
+            self.OutputAsCsv = val;
+        end
+        
+        function setOutputPath(self, val)
+            arguments
+                self;
+                val {mustBeFolder};
+            end
+            self.OutputPath = val;
+        end
+        
+    end
+    
+end
